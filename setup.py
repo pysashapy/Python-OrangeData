5f830bed3f88acd30d02bf5bc5280abab5c64c29
pysashapy <sasha.2000ibr@gmail.com>
2023-04-12 06:37:31 +0300

    ref

--- setup.py
+++ setup.py
@@ -2,12 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='Python-OrangeData',
-    version='0.0.1a',
+    version='0.0.2',
     packages=find_packages(),
-    url='',
-    license='',
     author='pysashapy',
-    author_email='',
-    description='',
-    requires=['requests', 'pycryptodome']
+    install_requires=open('requirements.txt', 'r').read().split()
 )
